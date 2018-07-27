3a293b01710c8cc0640cc6eb5e78f350fc3c2631
Dima Koval <dkoval@heliostech.fr>
2018-07-27 17:16:58 +0300

    Fix pusher

---
+++ app/api/api_v2/slanger.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module APIv2
+  class Slanger < Grape::API
+    desc 'Returns credentials used to subscribe to private Pusher channel. ' \
+      'IMPORTANT: Pusher events are not part of Peatio public interface. ' \
+      'The events may be changed or removed in further releases.'
+    params do
+      requires :channel_name, type: String, desc: 'The name of the channel being subscribed to.'
+      requires :socket_id, type: String, desc: 'An unique identifier for the connected client.'
+    end
+    post '/slanger/auth' do
+      sn = params[:channel_name].split('-', 2).last
+      if current_user.sn == sn
+        body ::Pusher[params[:channel_name]].authenticate(params[:socket_id])
+        status 200
+      else
+        status 422
+      end
+    end
+  end
+end
--- lib/engine.rb
+++ lib/engine.rb
@@ -11,11 +11,16 @@ module APILimitPlugin
     # Mount new API resource in hook (friendly with development environment).
     # More about configuration stages: http://guides.rubyonrails.org/configuring.html#configuring-action-dispatch
     config.after_initialize do
-      Rack::Attack.throttle 'Limit number of calls to API',
-        limit:  ENV.fetch('PEATIO_API_LIMIT_NUMBER', 6000).to_i,
-        period: ENV.fetch('PEATIO_API_LIMIT_PERIOD', 5.minutes).to_i do |req|
-          req.env['api_v2.authentic_member_email']
-        end
+      limit = ENV.fetch('PEATIO_API_LIMIT_NUMBER', 6000).to_i
+      period = ENV.fetch('PEATIO_API_LIMIT_PERIOD', 5.minutes).to_i
+
+      Rack::Attack.throttle 'Limit number of calls to API', limit: limit, period: period do |req|
+        req.env['api_v2.authentic_member_email']
+      end
+    end
+
+    config.to_prepare do
+      APIv2::Mount.mount APIv2::Slanger
     end
   end
 end
